42a53148133d0614949d47149b75258f8838758a
wang2004 <10935036@dcsh.tp.edu.tw>
2021-06-17 11:53:54 +0800

    修改、刪除投票主題，修改、刪除投票選項

--- default/urls.py
+++ default/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('poll/', views.PollList.as_view()), 
     path('poll/<int:pk>/', views.PollDetail.as_view()), 
     path('option/<int:oid>/', views.PollVote.as_view()),
-    path('poll/create/',views.PollCreate.as_view()),
-    path('poll/<int:pk>/edit/',views.PollEdit.as_view()),
-    path('poll/<int:pk>/delete/',views.PollDelete.as_view()),
+    path('poll/create/', views.PollCreate.as_view()),
+    path('poll/<int:pk>/edit/', views.PollEdit.as_view()),
+    path('poll/<int:pk>/delete/', views.PollDelete.as_view()),
+    path('poll/<int:pk>/add/', views.OptionCreate.as_view()),
+    path('option/<int:oid>/edit/', views.OptionEdit.as_view()),
+    path('option/<int:oid>/delete/', views.OptionDelete.as_view()),
 ]
--- default/views.py
+++ default/views.py
@@ -1,3 +1,4 @@
+from django.db.models.fields import Field
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, RedirectView,CreateView,UpdateView,DateDetailView
 from django.views.generic.edit import DeleteView
@@ -45,3 +46,31 @@ class PollEdit(UpdateView):
 class PollDelete(DeleteView):
     model = Poll
     success_url = '/poll/'
+
+class OptionCreate(CreateView):
+    model = Option
+    fields = ['title']
+    template_name = 'default/poll_form.html'
+
+    def form_valid(self, form):
+        form.instance.poll_id = self.kwargs['pk'] 
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return "/poll/{}/".format(self.object.poll_id)
+
+class OptionEdit(UpdateView):
+    model = Option
+    fields = ['title']
+    pk_url_kwarg = 'oid'
+    template_name = 'deefault/poll_form.html'
+
+    def get_success_url(self):
+        return "/poll/{}/".format(self.object.poll_id)
+
+class OptionDelete(DeleteView):
+    model = Option
+    pk_url_kwarg = 'oid'
+
+    def get_success_url(self):
+        return "/poll/{}/".format(self.object.poll_id)
