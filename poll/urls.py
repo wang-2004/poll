6a781bb4591d901d9096043927d094e3b6a21367
wang2004 <10935036@dcsh.tp.edu.tw>
2021-06-24 12:27:39 +0800

    修改空路徑

--- poll/urls.py
+++ poll/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
+from default import views
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -20,5 +22,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('default.urls')),
-    path('', RedirectView.as_view(url='/poll/')),
+    path('',RedirectView.as_view(url='poll/'))
 ]
